3af0a1fb5ab63a8ba6cc166dac8653a57bdac082
safistore <safeekestore@gmail.com>
2026-06-16 15:24:40 +0000

    zx

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
-function addVariationProduct(){
-
-    const size =
-    document.getElementById("size1").value;
-
-    const color =
-    document.getElementById("color1").value;
-
-    const product = {
-
-        id:1,
-
-        name:"Premium T-Shirt",
-
-        price:599,
-
-        image:"products/tshirt.jpg",
-
-        qty:1,
-
-        size:size,
-
-        color:color
-
-    };
-
-    let cart =
-    JSON.parse(
-    localStorage.getItem("cart")
-    ) || [];
-
-    cart.push(product);
-
-    localStorage.setItem(
-    "cart",
-    JSON.stringify(cart)
-    );
-
-    alert(
-    "Added To Cart!"
-    );
-
-    renderCart();
+const products = [
+{
+    id:1,
+    name:"Premium Watch",
+    price:999,
+    image:"products/watch.jpg",
+    description:"Elegant waterproof watch",
+    sizes:["Standard"],
+    colors:["Black","Silver"]
+},
+{
+    id:2,
+    name:"Fashion Bag",
+    price:799,
+    image:"products/bag.jpg",
+    description:"Stylish handbag",
+    sizes:["Small","Medium","Large"],
+    colors:["Black","Brown","Pink"]
+},
+{
+    id:3,
+    name:"Running Shoes",
+    price:1499,
+    image:"products/shoes.jpg",
+    description:"Comfortable sports shoes",
+    sizes:["7","8","9","10"],
+    colors:["Black","White","Blue"]
 }
+];
